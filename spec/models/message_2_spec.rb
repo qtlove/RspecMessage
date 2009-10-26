1c6ec0abff580c6acd0775a1fe1d762c7b502672
unknown <qixiang@.(none)>
2009-10-26 16:31:55 +0800

    modify and add new spec

--- spec/models/message_2_spec.rb
+++ spec/models/message_2_spec.rb
@@ -2,18 +2,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper' )
 
 describe Message do
   describe "validations" do
+    before(:each) do
+      @message = Message.new(:title => "foo" , :text => "bar" )
+    end
+
     it "should be valid with valid attributes" do
-      Message.new(:title => "foo", :text => "bar").should be_valid
+      @message.should be_valid
     end
 
     it "should not be valid without a title" do
-      message = Message.new :title => nil
-      message.should_not be_valid
+      @message.title = nil
+      @message.should_not be_valid
     end
 
     it "should not be valid without text" do
-      message = Message.new :text => nil, :title => "foo"
-      message.should_not be_valid
+      @message.text = nil
+      @message.should_not be_valid
     end
 
     it "should not be valid without a recipient"
